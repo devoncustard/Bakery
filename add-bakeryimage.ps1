62dde4c346f758d5fcaf18aab1ff2818c342114a
devoncustard <joe@mojo-online.net>
2014-11-17 09:17:34 +0000

    API Changes

--- add-bakeryimage.ps1
+++ add-bakeryimage.ps1
@@ -1,28 +1,27 @@
-param([string]$name,[string]$location,[string]$tag,[bool]$sysprep,[string]$provider,[string]$os,[string]$version)
-$conn=new-object system.data.sqlclient.sqlconnection
-$conn.connectionstring="Server=127.0.0.1;Database=Bakery;Trusted_Connection=true;"
+param([string]$id,[string]$location,[string]$commonname,[int]$imagetype,[int]$provider,[int]$os,[string]$version)
 
-$cmd=new-object System.data.sqlclient.sqlcommand
-$cmd.connection=$conn
-$conn.open()
-$text="insert into Images (Name,Location,SearchTag,Sysprep,BakedOn,Provider,OS_Family,OS_Version) Values(@name,@loc,@stag,@sysp,@bakedon,@provider,@os_family,@os_version)"
-$cmd.commandtext=$text
-$cmd.Parameters.Add((new-object Data.SqlClient.SqlParameter("@name",[Data.SQLDBType]::NVarChar,50))) >$null
-$cmd.Parameters.Add((new-object Data.SqlClient.SqlParameter("@loc",[Data.SQLDBType]::NVarChar,250))) >$null
-$cmd.Parameters.Add((new-object Data.SqlClient.SqlParameter("@stag",[Data.SQLDBType]::NVarChar,50))) >$null
-$cmd.Parameters.Add((new-object Data.SqlClient.SqlParameter("@sysp",[Data.SQLDBType]::Bit))) >$null
-$cmd.Parameters.Add((new-object Data.SqlClient.SqlParameter("@bakedon",[Data.SQLDBType]::DateTime))) >$null
-$cmd.Parameters.Add((new-object Data.SqlClient.SqlParameter("@provider",[Data.SQLDBType]::NVarChar,50))) >$null
-$cmd.Parameters.Add((new-object Data.SqlClient.SqlParameter("@os_family",[Data.SQLDBType]::NVarChar,50))) >$null
-$cmd.Parameters.Add((new-object Data.SqlClient.SqlParameter("@os_version",[Data.SQLDBType]::NVarChar,50))) >$null
+# test parameters
+$id=[Guid]::NewGuid().ToString().Replace("-","")
+$desc="test desc"
+$location="anywhere"
+$commonname="blahblah"
+$imagetype=1
+$bakedon=get-date
+$provider=1
+$osfamily=1
+$osversion="2008R2"
 
-$cmd.Parameters["@name"].Value=$name
-$cmd.Parameters["@loc"].Value=$location
-$cmd.Parameters["@stag"].Value=$tag
-$cmd.Parameters["@sysp"].Value=$sysprep
-$cmd.Parameters["@bakedon"].Value=Get-Date
-$cmd.Parameters["@provider"].Value=$provider
-$cmd.Parameters["@os_family"].Value=$os
-$cmd.Parameters["@os_version"].Value=$version
-$cmd.executenonquery() >$null
-$conn.close()
+$body=@{
+	Id=$id
+	Description=$desc
+	Location=$location
+	CommonName=$commonname
+	ImageType=$ImageType
+	Bakedon=$bakedon
+	Provider=$provider
+	OS_Family=$osfamily
+	OS_Version=$osversion
+	}
+
+
+invoke-restmethod http://localhost:65316/api/image -Method Post -Body $body
--- get-bakeryimage.ps1
+++ get-bakeryimage.ps1
@@ -1,35 +1,16 @@
-param([string]$Provider,[string]$Searchtag,[string]$sysprepped)
-#$conn=new-object system.data.sqlclient.sqlconnection
-#$conn.connectionstring="Server=127.0.0.1;Database=Bakery;Trusted_Connection=true;"
-#$cmd=new-object System.data.sqlclient.sqlcommand
-#$cmd.connection=$conn
-#$conn.open()
-#$cmd.commandtext="SELECT TOP 1 * FROM Images WHERE SearchTag='${tag}' AND SysPrep=${sysprepped} AND Provider='${provider}' ORDER BY BAKEDON DESC"
-#$ds=New-Object System.Data.Dataset
-#$da=New-Object System.Data.SqlClient.SqlDataAdapter($cmd)
-#$da.fill($ds) |Out-Null
-#$conn.close()
-#Return $ds.tables[0].rows[0]
+param([string]$Provider,[string]$ImageName,[string]$ImageType)
 
-$Provider="VAG"
-$SearchTag="W2008R2-BASE"
-
-$Sysprep=($sysprepped -EQ "true")
+#For testing
+$Provider=1
+$ImageName="blahblah"
+$ImageType=1
 
 
 Add-Type @"
-using System;public class ImageRequest{ public string SearchTag {get;set;}public bool Sysprep {get;set;} public string Provider {get;set;} } 
+using System;public class ImageRequest{ public string CommonName {get;set;}public int ImageType {get;set;} public string Provider {get;set;} } 
 "@
 
 
-$o=new-object ImageRequest
-$o.SearchTag=$Searchtag
-$o.Sysprep=($sysprepped -EQ "True")
-$o.Provider=$Provider
 
-$ht2=@{}
-
-$o.psobject.Properties |Foreach { $ht2[$_.Name]=$_.Value }
-
-$r=invoke-restmethod -URI "http://webbake/Bakery/api/image/?Sysprep=${Sysprep}&SearchTag=${Searchtag}&Provider=${Provider}" -Method Get
+$r=invoke-restmethod -URI "http://localhost:65316/api/image/?CommonName=${ImageName}&ImageType=${ImageType}&Provider=${Provider}" -Method Get
 $r
